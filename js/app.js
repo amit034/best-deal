cb730d2c9de5fa020f99a495c4383ad3082e63a7
Amit Rotbard <arotbard@interwise.com>
2015-03-29 19:53:39 +0300

    clean project

--- js/app.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by arotbard on 10-Mar-15.
- */
-
-var myApp = angular.module('bestDeal', []);
--- js/frameBuilder.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by arotbard on 10-Mar-15.
- */
-var iframe  = document.createElement ("iframe");
-iframe.src  = chrome.extension.getURL ("partials/rightFrame.html");
-document.body.appendChild(iframe);
